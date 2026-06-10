41f45ee7041550f61a36da6b3cc53b6f0252d416
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-06-10 23:10:16 +0500

    Реализован CRUD для курса через Viewsets. Протестировано Postman

--- materials/urls.py
+++ materials/urls.py
@@ -1,6 +1,12 @@
+from rest_framework.routers import DefaultRouter
 from materials.apps import MaterialsConfig
+from materials.views import CourseViewSet
 
 app_name = MaterialsConfig.name
 
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet, basename="courses")
 
 urlpatterns = []
+
+urlpatterns += router.urls
