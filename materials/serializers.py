b73bf919c21816f77c6e40a26ae850759954c15d
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-06-15 11:57:36 +0500

    Реализовано поле "количество уроков"

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,9 +1,23 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseDetailSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        if obj.lessons.all():
+            return obj.lessons.all().count()
+        return None
+
     class Meta:
         model = Course
         fields = '__all__'
