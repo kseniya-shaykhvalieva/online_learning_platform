c11657e5fba0d07205fa8a5982493baf19137fc7
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-06-18 14:06:43 +0500

    Реализован CRUD для пользователей

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,8 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
-from users.views import UserCreateView, PaymentViewSet, UserCreateAPIView
+from users.views import UserCreateView, PaymentViewSet, UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, \
+    UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
@@ -19,9 +20,13 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", UserCreateView.as_view(), name="register"),
-    path('api_login/', TokenObtainPairView.as_view(permission_classes = (AllowAny,)), name='api_login'),
-    path('token/refresh/', TokenRefreshView.as_view(permission_classes = (AllowAny,)), name='token_refresh'),
-    path('api_register/', UserCreateAPIView.as_view(), name='api_register'),
+    path('api_login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='api_login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path('user_create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('users_list/', UserListAPIView.as_view(), name='users_list'),
+    path('user_detail/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('user_update/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('user_delete/', UserDestroyAPIView.as_view(), name='user_delete'),
 ]
 
 urlpatterns += router.urls
