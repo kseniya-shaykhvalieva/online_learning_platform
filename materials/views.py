dd22d54321069f5080cb9ff1f7bee576e23d82f5
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-06-18 21:57:27 +0500

    Создана группа модераторов. Настроены права

--- materials/views.py
+++ materials/views.py
@@ -1,12 +1,21 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
+from users.permissions import IsModer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve", "update"]:
+            self.permission_classes = (IsModer,)
+        elif self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModer,)
+        return super().get_permissions()
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -17,22 +26,27 @@ class CourseViewSet(ModelViewSet):
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer,)
 
 
 class LessonCreateAPIView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, ~IsModer,)
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer,)
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer,)
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, ~IsModer,)
