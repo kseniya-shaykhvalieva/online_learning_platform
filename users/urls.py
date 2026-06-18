34533aa7c4673deb105745e6e88898ba9421eb7e
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-06-18 13:37:23 +0500

    Настроена регистрация, авторизация через DRF

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
-from users.views import UserCreateView, PaymentViewSet
+from users.views import UserCreateView, PaymentViewSet, UserCreateAPIView
 
 app_name = UsersConfig.name
-
 
 router = DefaultRouter()
 router.register(r"payment", PaymentViewSet, basename="payment")
@@ -15,6 +19,9 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", UserCreateView.as_view(), name="register"),
+    path('api_login/', TokenObtainPairView.as_view(permission_classes = (AllowAny,)), name='api_login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes = (AllowAny,)), name='token_refresh'),
+    path('api_register/', UserCreateAPIView.as_view(), name='api_register'),
 ]
 
 urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters
 
 from users.forms import UserRegisterForm
 from users.models import CustomUser, Payment
-from users.serializers import PaymentSerializer
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class UserCreateView(CreateView):
@@ -22,3 +24,14 @@ class PaymentViewSet(ModelViewSet):
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
     ordering_fields = ("pay_date",)
     filterset_fields = ("course", "lesson", "pay_method",)
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
