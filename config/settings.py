a1c7043f2c0427ceab98fd0ac9adb7bd728decea
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-07-14 21:15:50 +0500

    Исправлены мелкие недочеты для тестов

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -154,3 +155,11 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": timedelta(days=1),
     },
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',
+        }
+    }
